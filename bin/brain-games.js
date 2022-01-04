5802581bce385b0f3ec6eee42ad23bfe5fd0d980
Leonid S Kucheruk <shtormlbt@mail.ru>
2022-01-04 18:59:34 +0500

    add src/cli.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import hellow from '../src/cli.js'
+
 console.log('Welcome to the Brain Games!');
+hellow();
---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readLineSync from 'readline-sync';
+
+export default () => { 
+    const name = readLineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}`);
+};
