61c93a4df8e5413eb6d31d5edac2eaf70ee90942
Leonid S Kucheruk <shtormlbt@mail.ru>
2022-01-06 17:05:49 +0500

    add hellow

--- src/gameEvent.js
+++ src/gameEvent.js
@@ -1,9 +1,8 @@
 import readLineSync from 'readline-sync';
+import hellow from './hellow.js';
 
 export default () => {
-  const name = readLineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const name = hellow();
   let win = true;
   for (let i = 0; i < 3; i += 1) {
     const rndNum = Math.floor(Math.random() * 1000);
