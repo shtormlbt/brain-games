73d7b7bffd25cba5651cd94d47f57a3b16264f03
Leonid S Kucheruk <shtormlbt@mail.ru>
2022-01-06 17:14:11 +0500

    add question

--- src/gameEvent.js
+++ src/gameEvent.js
@@ -1,26 +1,20 @@
-import readLineSync from 'readline-sync';
 import hellow from './hellow.js';
+import question from './question.js';
 
 export default () => {
   const name = hellow();
   let win = true;
   for (let i = 0; i < 3; i += 1) {
     const rndNum = Math.floor(Math.random() * 1000);
-    console.log(`Question: ${rndNum}`);
-    const answer = readLineSync.question('Your answer: ');
-    if (rndNum % 2 === 0 && answer !== 'yes') {
-      console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
-      console.log(`Let's try again, ${name}`);
-      win = false;
-      break;
-    } else
-    if (rndNum % 2 !== 0 && answer !== 'no') {
-      console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
-      console.log(`Let's try again, ${name}`);
-      win = false;
-      break;
+    let expected = '';
+    if (rndNum % 2 === 0) {
+      expected = 'yes';
     } else {
-      console.log('Correct!');
+      expected = 'no';
+    }
+    win = question(rndNum, expected, name);
+    if (win === false) {
+      break;
     }
   }
   if (win) {
